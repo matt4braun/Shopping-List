4e5446b260696935f45403e2f16749086278e3d7
Matthias Braun <167146329+matt4braun@users.noreply.github.com>
2025-01-23 18:06:23 -0500

    Update shoppingcart.py
    adding comments

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,9 +1,9 @@
 again = True
-item = ""
+item = ""    #list of items
 price = 0
 while again == True:
-    item = item + ", "+input("what item do you want to add")
-    price=price+int(input("how much does it cost"))
+    item = item + ", "+input("what item do you want to add")    #adding items to list
+    price=price+int(input("how much does it cost"))        #adding to price
     if input("y or n, do you want to add another item") != 'y':
         print("here")
         again = False
